763c832e16e3b9015a6567853dc47d47cd68cfb9
“Santiago <santiago.mickley@utec.edu.pe>
2021-05-14 22:34:19 -0500

    Programa que usa operadores logícos

---
+++ Semana02/bitslog.py
@@ -0,0 +1,6 @@
+x = int(input("Ingrese un número par que este entre 10 y 20: "))
+
+if 10 < x <20 and x % 2 == 0:
+    print(x, "Cumple con las condidciones del programa")
+else:
+    print(x, "No cumple con las condiciones del programa")
